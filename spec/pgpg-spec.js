29a2e8776b313fef2fa974b3eaddd09bd314acdb
kaidowk11 <kaidowk11@gmail.com>
2022-10-04 21:49:33 +0700

    q

---
+++ spec/pgpg-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import Pgpg from '../lib/pgpg';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Pgpg', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('pgpg');
+  });
+
+  describe('when the pgpg:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.pgpg')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'pgpg:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.pgpg')).toExist();
+
+        let pgpgElement = workspaceElement.querySelector('.pgpg');
+        expect(pgpgElement).toExist();
+
+        let pgpgPanel = atom.workspace.panelForItem(pgpgElement);
+        expect(pgpgPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'pgpg:toggle');
+        expect(pgpgPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.pgpg')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'pgpg:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let pgpgElement = workspaceElement.querySelector('.pgpg');
+        expect(pgpgElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'pgpg:toggle');
+        expect(pgpgElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/pgpg-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PgpgView from '../lib/pgpg-view';
+
+describe('PgpgView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
